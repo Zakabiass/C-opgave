aee5808166eae6953dd198401c5120961ab4940e
Zakabiass <141635072+Zakabiass@users.noreply.github.com>
2023-08-10 13:04:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp1
+    // This code is supost to create a replica of the amarican flag 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writes all the white and red lines
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                      \n" + "                                      ");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                      \n" + "                                      ");
+            {
+                //Writes the blue and adds the stars
+                Console.SetCursorPosition(0,0);
+                Console.BackgroundColor= ConsoleColor.DarkBlue;
+                Console.WriteLine("                       \n" + " *   *   *   *   *   * ");
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("   *   *   *   *   *   \n" + " *   *   *   *   *   * ");
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("   *   *   *   *   *   \n" + " *   *   *   *   *   * ");
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("   *   *   *   *   *   \n" + " *   *   *   *   *   * ");
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("   *   *   *   *   *   \n" + " *   *   *   *   *   * ");
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("                       ");
+                {
+                    //This part removes the big cloud of text at the bottom of the console
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
