d26fc684b083bec159a66ae18cd905a0bec9845d
Zakabiass <141635072+Zakabiass@users.noreply.github.com>
2023-08-10 16:28:08 +0200

    Add files via upload

---
+++ Program til rumfang.cs
@@ -0,0 +1,27 @@
+﻿// Rumfanget
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This part is for the diffrent values
+            float L;
+            float B;
+            float H;
+            float R;
+
+            // This part is for the user to chose the length, width and hight of the box
+            Console.Write("What is the length?: ");
+            L = float.Parse(Console.ReadLine());
+            Console.Write("What is the width?: ");
+            B = float.Parse(Console.ReadLine());
+            Console.Write("What is the hight?: ");
+            H = float.Parse(Console.ReadLine());
+
+            // And down here it calculates it and gives the resault
+            R = L * B * H;
+            Console.WriteLine($"\nThe volum of the box is {R}");
+        }
+    }
+}
