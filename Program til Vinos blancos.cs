d4b9b64bd27b86e817d9004137ba5c44a3cc0537
Zakabiass <141635072+Zakabiass@users.noreply.github.com>
2023-08-15 20:59:38 +0200

    Program til Vinos blancos.cs

--- Program til Vinos blancos.cs
+++ Program til Vinos blancos.cs
@@ -17,6 +17,7 @@ namespace Vinos_blancos
             int[] arrayVine = { 175134, 175388, 172818, 142709, 151437, 152620, 150979, 152210, 149450, 154398, 150160 };
             // This part sorts the array
             Array.Sort(arrayVine);
+            Array.Reverse(arrayVine);
 
             // This loop prints out stars for all the vine sold
             for (int i = 0; i < arrayVine.Length; i++)
